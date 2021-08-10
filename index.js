e2d03ae496c58492d609aaae951ef2be88c55bba
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-08-10 21:30:40 +0200

    solve all iterations except 1st 1 test

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, cur) => acc + cur, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
